35d4c95bde6cd57f923d2e51a6c05a19e3f4d6ce
Ezatullah <ezatullahahmadi2023f@gmail.com>
2025-05-15 16:58:00 +0430

    hero section designed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,37 @@
 import React from "react";
-
+import profileImg from "../assets/ezat.jpeg";
 function Hero() {
   return (
-    <section id="hero" className="min-h-s">
-      <h1>Hero section</h1>
+    <section
+      id="hero"
+      className="min-h-screen flex flex-col md:flex-row items-center justify-center gap-12 bg-gray-300 px-6 md:px-16"
+    >
+      <div className="max-w-xl text-center md:text-left">
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">
+          Hi,I'm <span className="text-blue-900">Ezatullah</span>{" "}
+        </h1>
+        <p className="text-lg md:text-xl text-gray-600 mb-8">
+          I’m a passionate front-end developer who loves turning ideas into
+          beautiful and responsive websites. I specialize in building modern
+          user interfaces with React.js and Tailwind CSS, focusing on clean
+          code, accessibility, and performance.I’m currently seeking
+          opportunities to work with creative teams and contribute to real-world
+          web projects.
+        </p>
+        <a
+          href="#projects"
+          className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-8 rounded-2xl shadow md:px-12 transition duration-300"
+        >
+          Projects
+        </a>
+      </div>
+      <div className="mb-8 md:mb-0 flex-shrink-0">
+        <img
+          src={profileImg}
+          alt="my img"
+          className="w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 rounded-full  shadow-lg"
+        />
+      </div>
     </section>
   );
 }
